17dd660bbfed266c2e6bbf11d69b6f8dfdec0e8d
sumerujhaveri <129935714+sumerujhaveri@users.noreply.github.com>
2023-04-08 12:32:48 +0530

    Create QuestionAnswer.java

---
+++ QuestionAnswer.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+public class QuestionAnswer {
+    public static String question[] ={
+            "Who created JAVA?",
+            "Which of these is not a programming language?",
+            "Android is..",
+            "What does API stand for?"
+    };
+    public static String choices[][] = {
+            {"James Gosling","Guido van Rossum","Tim Berners-Lee","Bjarn Stroustrup"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"a web browser","an operating system","a web server","none of the above"},
+            {"Application Programming Interface","Android Programming Interface","Android Page Interface","Application Page Interface"}
+    };
+    public static String correctAnswers[] = {
+            "James Gosling",
+            "Notepad",
+            "an operating system",
+            "Application Programming Interface"
+    };}
